fbb649f409620a55ae48f1bffd655c5a04489371
DeaDDooMER <deaddoomer@deadsoftware.ru>
2021-06-17 19:52:30 +0300

    cleanup: clean boot sequence

--- src/game/g_menu.pas
+++ src/game/g_menu.pas
@@ -3531,16 +3531,11 @@ end;
 procedure g_Menu_Init();
 begin
   MenuLoadData();
-  g_GUI_Init();
   CreateAllMenus();
 end;
 
 procedure g_Menu_Free();
 begin
-  g_GUI_Destroy();
-
-  e_WriteLog('Releasing menu data...', TMsgType.Notify);
-
   MenuFreeData();
 end;
 
